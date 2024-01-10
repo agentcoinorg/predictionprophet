1949cf8fb778ff691ff53cba207d7e8d82c01e98
Nestor Amesty <nestor09amesty@gmail.com>
2024-01-10 22:17:52 +0100

    Parametrization of research function constants

--- evo_researcher/functions/research.py
+++ evo_researcher/functions/research.py
@@ -2,18 +2,17 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from evo_researcher.functions.create_embeddings_from_results import create_embeddings_from_results
 from evo_researcher.functions.generate_subqueries import generate_subqueries
 from evo_researcher.functions.prepare_report import prepare_report
-from evo_researcher.functions.rerank_results import rerank_results
 
 from evo_researcher.functions.rerank_subqueries import rerank_subqueries
 from evo_researcher.functions.scrape_results import scrape_results
 from evo_researcher.functions.search import search
 
-def research(goal: str) -> tuple[str, str]:
-    initial_subqueries_limit = 20
-    subqueries_limit = 4
-    scrape_content_split_chunk_size = 800
-    scrape_content_split_chunk_overlap = 225
-    top_k = 8
+def research(goal: str, **kwargs) -> tuple[str, str]:
+    initial_subqueries_limit = kwargs.get('initial_subqueries_limit', 20)
+    subqueries_limit = kwargs.get('subqueries_limit', 4)
+    scrape_content_split_chunk_size = kwargs.get('scrape_content_split_chunk_size', 800)
+    scrape_content_split_chunk_overlap = kwargs.get('scrape_content_split_chunk_overlap', 225)
+    top_k_per_query = kwargs.get('top_k_per_query', 8)
 
     queries = generate_subqueries(query=goal, limit=initial_subqueries_limit)
     queries = rerank_subqueries(queries=queries, goal=goal)[:subqueries_limit]
@@ -21,9 +20,7 @@ def research(goal: str) -> tuple[str, str]:
     search_results_with_queries = search(queries, lambda result: not result["url"].startswith("https://www.youtube"))
 
     scrape_args = [result for (_, result) in search_results_with_queries]
-
     scraped = scrape_results(scrape_args)
-
     scraped = [result for result in scraped if result.content != ""]
 
     text_splitter = RecursiveCharacterTextSplitter(
@@ -31,14 +28,14 @@ def research(goal: str) -> tuple[str, str]:
         chunk_size=scrape_content_split_chunk_size,
         chunk_overlap=scrape_content_split_chunk_overlap
     )
-    print (f"Scraped {len(scraped)} results")
+
     collection = create_embeddings_from_results(scraped, text_splitter)
 
     vector_result_texts: list[str] = []
 
     for query in queries:
-        top_k_results = collection.similarity_search(query, k=top_k)
-        vector_result_texts += [f"{result.page_content}" for result in top_k_results]
+        top_k_per_query_results = collection.similarity_search(query, k=top_k_per_query)
+        vector_result_texts += [f"{result.page_content}" for result in top_k_per_query_results]
     
     chunks = ""
     for chunk in vector_result_texts:
