aa7635b32d81de2d4063f240a82aab26c738772a
Peter Jung <peter@jung.ninja>
2024-02-02 08:36:07 +0100

    Switch from DISABLE_CACHE to ENABLE_CACHE

--- evo_researcher/functions/cache.py
+++ evo_researcher/functions/cache.py
@@ -2,9 +2,12 @@ import os
 from typing import TypeVar, Callable, Any, cast
 from joblib import Memory
 from functools import cache
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MEMORY = Memory("./.cachedir", verbose=0)
-DISABLE_CACHE = os.getenv("DISABLE_CACHE", "0") == "1"
+ENABLE_CACHE = os.getenv("ENABLE_CACHE", "0") == "1"
 
 T = TypeVar("T", bound=Callable[..., Any])
 
@@ -13,4 +16,4 @@ def persisted_inmemory_cache(func: T) -> T:
     """
     Wraps a function with both file cache (for persistent cache) and in-memory cache (for speed).
     """
-    return cast(T, cache(MEMORY.cache(func)) if not DISABLE_CACHE else func)
+    return cast(T, cache(MEMORY.cache(func)) if ENABLE_CACHE else func)
