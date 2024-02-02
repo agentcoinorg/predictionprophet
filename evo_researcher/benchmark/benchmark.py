071bbec5f122e82c5288070267d71d5081ba740e
Peter Jung <peter@jung.ninja>
2024-02-02 15:28:05 +0100

    time only if not cache

--- evo_researcher/benchmark/benchmark.py
+++ evo_researcher/benchmark/benchmark.py
@@ -21,6 +21,7 @@ from evo_researcher.benchmark.utils import (
     get_llm_api_call_cost,
     get_markets,
 )
+from evo_researcher.functions.cache import ENABLE_CACHE
 
 
 class Benchmarker:
@@ -95,7 +96,8 @@ class Benchmarker:
                     prediction = agent.research_and_predict(
                         market_question=market.question
                     )
-                    prediction.time = time.time() - start
+                    # Set time only if we aren't using cache, otherwise it won't be accurate. 
+                    prediction.time = time.time() - start if not ENABLE_CACHE else None
 
                     if cb.total_tokens > 0 and cb.total_cost == 0:
                         # TODO: this is a hack to get the cost for an unsupported model
