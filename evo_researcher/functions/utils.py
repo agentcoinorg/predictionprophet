e7af3bdd8e956993845493f3dcff7b414a2f1bef
Peter Jung <peter@jung.ninja>
2024-02-12 14:10:27 +0100

    Simplify

--- evo_researcher/functions/utils.py
+++ evo_researcher/functions/utils.py
@@ -1,9 +1,6 @@
 import tiktoken
 
 
-def trim_to_n_tokens(content: str, n: int, model: str, trim_batch_size: int = 128) -> str:
+def trim_to_n_tokens(content: str, n: int, model: str) -> str:
     encoder = tiktoken.encoding_for_model(model)
-    encoded = encoder.encode(content)
-    while len(encoded) > n:
-        encoded = encoded[:-trim_batch_size]
-    return encoder.decode(encoded)
+    return encoder.decode(encoder.encode(content)[:n])
