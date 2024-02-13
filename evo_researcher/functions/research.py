44aa36c94e512f11b815976b8d081ae518c2b79d
Peter Jung <peter@jung.ninja>
2024-02-13 09:08:18 +0100

    revert initial_subqueries_limit

--- evo_researcher/functions/research.py
+++ evo_researcher/functions/research.py
@@ -13,7 +13,7 @@ def research(
     openai_key: str,
     use_summaries: bool,
     model: str = "gpt-4-1106-preview",
-    initial_subqueries_limit: int = 4,
+    initial_subqueries_limit: int = 20,
     subqueries_limit: int = 4,
     scrape_content_split_chunk_size: int = 800,
     scrape_content_split_chunk_overlap: int = 225,
