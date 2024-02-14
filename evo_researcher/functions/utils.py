40b81a71b229d80588771e8a664368097f493ee8
Peter Jung <peter@jung.ninja>
2024-02-14 11:11:47 +0100

    Add precision and recall to the benchmark (#35)

--- evo_researcher/functions/utils.py
+++ evo_researcher/functions/utils.py
@@ -52,4 +52,3 @@ def should_not_happen(
 def trim_to_n_tokens(content: str, n: int, model: str) -> str:
     encoder = tiktoken.encoding_for_model(model)
     return encoder.decode(encoder.encode(content)[:n])
- 
