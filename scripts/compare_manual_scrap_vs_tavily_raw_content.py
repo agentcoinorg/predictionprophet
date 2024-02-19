6d43409efc4decb06eff9c382fa3e29a4228f598
Peter Jung <peter@jung.ninja>
2024-02-19 10:33:54 +0100

    Compare Tavily's scraping vs. our scraping

---
+++ scripts/compare_manual_scrap_vs_tavily_raw_content.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from dotenv import load_dotenv
+from evo_researcher.functions.web_search import web_search
+from evo_researcher.functions.scrape_results import scrape_results
+
+load_dotenv()
+st.set_page_config(layout="wide")
+
+query = st.text_input("Enter a query")
+
+if not query:
+    st.warning("Please enter a query")
+    st.stop()
+
+search = web_search(query)
+scrape = scrape_results(search)
+
+index = int(st.number_input("Index", min_value=0, max_value=len(search) - 1, value=0))
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("### Tavily's raw content")
+    st.write(search[index].url)
+    st.write(f"{len(search[index].raw_content.split())} words")
+    st.markdown("---")
+    st.write(search[index].raw_content)
+
+with col2:
+    st.markdown("### Scraped content")
+    st.write(scrape[index].url)
+    st.write(f"{len(scrape[index].content.split())} words")
+    st.markdown("---")
+    st.write(scrape[index].content)
