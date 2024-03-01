0825af074fac49ba8d84d7b51b6ae8f15b02deea
Peter Jung <peter@jung.ninja>
2024-03-01 13:26:26 +0100

    remove unnecessary condition

--- evo_researcher/deployment/models.py
+++ evo_researcher/deployment/models.py
@@ -29,15 +29,13 @@ class DeployableAgentER(DeployableAgent):
             # Manifold won't give us fractional Mana, so bet the minimum amount to win at least 1 Mana.
             amount = market.get_minimum_bet_to_win(answer, amount_to_win=1) 
             max_bet_amount = 10.0
-        elif isinstance(market, OmenAgentMarket):
+        else:
             # Otherwise, bet to win at least 0.001 (of something), unless the bet would be less than the tiny bet.
             amount = max(
                 Decimal(minimum_bet_to_win(answer, amount_to_win=0.001, market=market)), 
                 market.get_tiny_bet_amount().amount,
             )
             max_bet_amount = 0.1
-        else:
-            raise ValueError(f"Unknow market: {market}")
         if amount > max_bet_amount:
             print(f"Would need at least {amount} {market.currency} to be profitable, betting only {market.get_tiny_bet_amount()} for benchmark purposes.")
             amount = market.get_tiny_bet_amount().amount
