f992eac725baf25ae6f46ed55a87e020d30fe849
Peter Jung <peter@jung.ninja>
2024-03-01 11:03:47 +0100

    update to support latest changes in pmat pr

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,10 +4,12 @@ import typing as t
 from git import Repo
 from datetime import datetime
 from prediction_market_agent_tooling.markets.markets import MarketType
-from prediction_market_agent_tooling.deploy.agent import MonitorConfig
-from prediction_market_agent_tooling.deploy.gcp.utils import gcp_get_secret_value
 from evo_researcher.deployment.models import DeployableAgentER, DeployableAgentER_EvoGPT3, DeployableAgentER_OlasEmbeddingOA
-from prediction_market_agent_tooling.markets.manifold.api import get_authenticated_user
+from prediction_market_agent_tooling.config import APIKeys
+from prediction_market_agent_tooling.gtypes import PrivateKey
+from prediction_market_agent_tooling.tools.web3_utils import verify_address
+from pydantic.types import SecretStr
+
 
 DEPLOYABLE_AGENTS = [
     DeployableAgentER_EvoGPT3,
@@ -38,25 +40,21 @@ def deploy(
     chosen_agent_class().deploy_gcp(
         repository=f"git+https://github.com/polywrap/evo.researcher.git@{Repo('.').active_branch.name}",
         market_type=market_type,
+        api_keys=APIKeys(
+            BET_FROM_ADDRESS=verify_address(bet_from_address),
+            MANIFOLD_API_KEY=SecretStr(f"{manifold_api_key_secret_name}:latest"),
+            BET_FROM_PRIVATE_KEY=PrivateKey(f"{bet_from_private_key_secret_name}:latest"),
+        ),
         memory=1024,
         labels={
             "owner": getpass.getuser().lower(),
         },
-        env_vars={
-            "BET_FROM_ADDRESS": bet_from_address,
-        },
         secrets={
-            "MANIFOLD_API_KEY": f"{manifold_api_key_secret_name}:latest",
             "OPENAI_API_KEY": f"{openai_api_key_secret_name}:latest",
             "TAVILY_API_KEY": f"{tavity_api_key_secret_name}:latest",
-            "BET_FROM_PRIVATE_KEY": f"{bet_from_private_key_secret_name}:latest",
         },
         cron_schedule="0 */2 * * *",
-        monitor_config=MonitorConfig(
-            start_time=start_time or datetime.utcnow(),
-            manifold_user_id=get_authenticated_user(gcp_get_secret_value(manifold_api_key_secret_name)).id,
-            omen_public_key=bet_from_address,
-        ),
+        start_time=start_time,
     )
 
 
