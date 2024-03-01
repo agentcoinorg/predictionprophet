8565cc0c2cc54d9e2753df981ad853f5ac3b2988
Peter Jung <peter@jung.ninja>
2024-03-01 12:12:32 +0100

    debug

--- evo_researcher/deployment/models.py
+++ evo_researcher/deployment/models.py
@@ -3,6 +3,7 @@ from evo_researcher.benchmark.agents import EvoAgent, OlasAgent, EmbeddingModel
 from prediction_market_agent_tooling.benchmark.agents import AbstractBenchmarkedAgent
 from prediction_market_agent_tooling.markets.agent_market import AgentMarket
 from prediction_market_agent_tooling.markets.manifold.manifold import ManifoldAgentMarket
+from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket
 from prediction_market_agent_tooling.deploy.agent import DeployableAgent, BetAmount
 from prediction_market_agent_tooling.markets.betting_strategies import minimum_bet_to_win
 
@@ -26,12 +27,14 @@ class DeployableAgentER(DeployableAgent):
         if isinstance(market, ManifoldAgentMarket) :
             # Manifold won't give us fractional Mana, so bet the minimum amount to win at least 1 Mana.
             amount = market.get_minimum_bet_to_win(answer, amount_to_win=1) 
-        else:
+        elif isinstance(market, OmenAgentMarket):
             # Otherwise, bet to win at least 0.001 (of something), unless the bet would be less than the tiny bet.
             amount = max(
                 Decimal(minimum_bet_to_win(answer, amount_to_win=0.001, market=market)), 
                 market.get_tiny_bet_amount().amount,
             )
+        else:
+            raise ValueError(f"Unknow market: {market}")
         return BetAmount(amount=amount, currency=market.currency)
 
     def answer_binary_market(self, market: AgentMarket) -> bool:
