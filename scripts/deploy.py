e90f9889bfcaa8a37c16caa70c841b2811c764fa
Peter Jung <peter@jung.ninja>
2024-03-05 13:57:17 +0100

    PMAT with monitored deployment (#42)

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,14 @@
 import typer
 import getpass
 import typing as t
+from git import Repo
+from datetime import datetime
 from prediction_market_agent_tooling.markets.markets import MarketType
 from evo_researcher.deployment.models import DeployableAgentER, DeployableAgentER_EvoGPT3, DeployableAgentER_OlasEmbeddingOA
+from prediction_market_agent_tooling.config import APIKeys
+from prediction_market_agent_tooling.gtypes import PrivateKey
+from prediction_market_agent_tooling.tools.web3_utils import verify_address
+from pydantic.types import SecretStr
 
 
 DEPLOYABLE_AGENTS = [
@@ -19,6 +25,7 @@ def deploy(
     tavity_api_key_secret_name: str = typer.Option(),
     bet_from_address: str = typer.Option(),
     bet_from_private_key_secret_name: str = typer.Option(),
+    start_time: datetime | None = None,
 ) -> None:
     """
     Script to execute locally to deploy the agent to GCP.
@@ -31,23 +38,23 @@ def deploy(
     chosen_agent_class: t.Type[DeployableAgentER] = [agent for agent in DEPLOYABLE_AGENTS if agent.__name__ == deployable_agent_name][0]
     
     chosen_agent_class().deploy_gcp(
-        repository="git+https://github.com/polywrap/evo.researcher.git@peter/new-deployment",
+        repository=f"git+https://github.com/polywrap/evo.researcher.git@{Repo('.').active_branch.name}",
         market_type=market_type,
+        api_keys=APIKeys(
+            BET_FROM_ADDRESS=verify_address(bet_from_address),
+            MANIFOLD_API_KEY=SecretStr(f"{manifold_api_key_secret_name}:latest"),
+            BET_FROM_PRIVATE_KEY=PrivateKey(f"{bet_from_private_key_secret_name}:latest"),
+        ),
         memory=1024,
         labels={
             "owner": getpass.getuser().lower(),
-            "deployable_agent_name": deployable_agent_name.lower(),
-        },
-        env_vars={
-            "BET_FROM_ADDRESS": bet_from_address,
         },
         secrets={
-            "MANIFOLD_API_KEY": f"{manifold_api_key_secret_name}:latest",
             "OPENAI_API_KEY": f"{openai_api_key_secret_name}:latest",
             "TAVILY_API_KEY": f"{tavity_api_key_secret_name}:latest",
-            "BET_FROM_PRIVATE_KEY": f"{bet_from_private_key_secret_name}:latest",
         },
         cron_schedule="0 */2 * * *",
+        start_time=start_time,
     )
 
 
