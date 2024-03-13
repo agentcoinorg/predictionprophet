cc7b04d19e2a1e47f70c47223d055d84b3332ee5
Nestor Amesty <nestor09amesty@gmail.com>
2024-03-13 20:01:20 +0100

    Better logging

--- evo_prophet/main.py
+++ evo_prophet/main.py
@@ -1,3 +1,4 @@
+import logging
 import click
 import time
 from dotenv import load_dotenv
@@ -6,6 +7,7 @@ from langchain_community.callbacks import get_openai_callback
 from evo_prophet.functions.research import research as evo_research
 
 load_dotenv()
+logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(message)s')
 
 def create_output_file(info: str, path: str) -> None:
     with open(path, 'w') as file:
@@ -58,14 +60,19 @@ def predict(prompt: str, path: str | None = None) -> None:
         if path:
             report = read_text_file(path)
         else:
-            report = evo_research(goal=prompt, model="gpt-4-0125-preview", use_summaries=False)
+            logger = logging.getLogger("research")
+            logger.setLevel(logging.INFO)
+            report = evo_research(goal=prompt, model="gpt-4-0125-preview", use_summaries=False, logger=logger)
         
         prediction = _make_prediction(market_question=prompt, additional_information=report, engine="gpt-4-0125-preview", temperature=0.0)
 
     end = time.time()
     
-    print(prediction)
-    print(f"\n\nTime elapsed: {end - start}\n\n{cb}\n\n")
+    if prediction.outcome_prediction == None:
+        raise("The agent failed to generate a prediction")
+    
+    print(f"\n\nQuestion: '{prompt}'\nProbability of ocurring: {prediction.outcome_prediction.p_yes * 100}%\nConfidence in prediction: {prediction.outcome_prediction.confidence * 100}%\nTime elapsed: {end - start}")
+    
 
 
 if __name__ == '__main__':
