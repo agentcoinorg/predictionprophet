fead1ad9522cf21833007048e910c937bec4a490
Peter Jung <peter@jung.ninja>
2024-03-22 09:59:29 +0100

    Answer more markets in Gnosis' deployable agents

--- prediction_prophet/deployment/models.py
+++ prediction_prophet/deployment/models.py
@@ -36,6 +36,7 @@ class DeployableAgentER(DeployableAgent):
         """
         Testing mode: Pick only one predictable market or nothing.
         """
+        picked_markets: list[AgentMarket] = []
         for market in markets:
             print(f"Looking if we recently bet on '{market.question}'.")
             if self.recently_betted(market):
@@ -44,8 +45,11 @@ class DeployableAgentER(DeployableAgent):
             print(f"Verifying market predictability for '{market.question}'.")
             if self.agent.is_predictable(market.question):
                 print(f"Market '{market.question}' is predictable.")
-                return [market]
-        return []
+                picked_markets.append(market)
+            # Hard-coded limit for testing purposes.
+            if len(picked_markets) >= 10:
+                break
+        return picked_markets
     
     def calculate_bet_amount(self, answer: bool, market: AgentMarket) -> BetAmount:
         amount: Decimal
@@ -69,7 +73,8 @@ class DeployableAgentER(DeployableAgent):
     def answer_binary_market(self, market: AgentMarket) -> bool:
         prediciton = self.agent.predict(market.question)  # Already checked in the `pick_markets`.
         if prediciton.outcome_prediction is None:
-            raise ValueError(f"Missing prediction: {prediciton}")
+            print("Error: Prediction failed.")  # When switched to proper logging, use error log.
+            return None
         binary_answer: bool = prediciton.outcome_prediction.p_yes > 0.5
         print(f"Answering '{market.question}' with '{binary_answer}'.")
         return binary_answer
