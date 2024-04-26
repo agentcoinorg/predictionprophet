50d9391e3f93b56546792a4e3e808ecaf2d11df3
Cesar <cesarbrazon10@gmail.com>
2024-04-26 20:06:25 +0200

    chore: handle prediction is async

--- prediction_prophet/bot.py
+++ prediction_prophet/bot.py
@@ -50,11 +50,94 @@ if discord_bot_token == None:
     raise Exception("No discord bot token provided")
 
 
+async def handle_prediction(goal: str, message: discord.Message, message_prophet: str):
+    message_prophet = message_prophet + "\n### Generating subqueries..."
+    await message.edit(content=message_prophet)
+    queries = generate_subqueries(
+        query=goal, limit=initial_subqueries_limit, model=model
+    )
+    message_prophet = message_prophet + "\n### Reranking subqueries..."
+    await message.edit(content=message_prophet)
+    queries = (
+        rerank_subqueries(queries=queries, goal=goal, model=model)[:subqueries_limit]
+        if initial_subqueries_limit > subqueries_limit
+        else queries
+    )
+
+    message_prophet = message_prophet + "\n### Searching the web..."
+    await message.edit(content=message_prophet)
+    search_results_with_queries = search(
+        queries,
+        lambda result: not result.url.startswith("https://www.youtube"),
+        tavily_api_key=tavily_api_key,
+    )
+
+    if not search_results_with_queries:
+        message_prophet = message_prophet = "\n### No search results found for goal"
+        await message.edit(content=message_prophet)
+        return
+
+    message_prophet = message_prophet + "\n### Scraping web results..."
+    await message.edit(content=message_prophet)
+    scrape_args = [result for (_, result) in search_results_with_queries]
+
+    scraped = scrape_results(scrape_args)
+    scraped = [result for result in scraped if result.content != ""]
+
+    message_prophet = message_prophet + "\n### Performing similarity searches..."
+    await message.edit(content=message_prophet)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", ". ", "  "],
+        chunk_size=scrape_content_split_chunk_size,
+        chunk_overlap=scrape_content_split_chunk_overlap,
+    )
+    collection = create_embeddings_from_results(scraped, text_splitter)
+
+    vector_result_texts: list[str] = []
+    for query in queries:
+        top_k_per_query_results = collection.similarity_search(query, k=top_k_per_query)
+        vector_result_texts += [
+            result.page_content
+            for result in top_k_per_query_results
+            if result.page_content not in vector_result_texts
+        ]
+
+    message_prophet = message_prophet + "\n### Preparing report..."
+    await message.edit(content=message_prophet)
+
+    report = prepare_report(goal, vector_result_texts, model=model)
+
+    message_prophet = message_prophet + "\n### Making prediction..."
+    await message.edit(content=message_prophet)
+
+    prediction = _make_prediction(
+        market_question=goal,
+        additional_information=report,
+        engine=model,
+        temperature=0.0,
+    )
+
+    if prediction.outcome_prediction == None:
+        message_prophet = (
+            message_prophet + "\n### The agent failed to generate a prediction"
+        )
+        await message.edit(content=message_prophet)
+        return
+
+    outcome_prediction = cast(OutcomePrediction, prediction.outcome_prediction)
+    message_prophet = (
+        message_prophet
+        + f"\n# Probability: {outcome_prediction.p_yes * 100}%. Confidence: {outcome_prediction.confidence * 100}%"
+    )
+    await message.edit(content=message_prophet)
+
+
 class ProphetClient(discord.Client):
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         """
         TODO:
             - Add channel ID
@@ -74,97 +157,10 @@ class ProphetClient(discord.Client):
                     content=f"The agent thinks this question is not predictable: \n{reasoning}"
                 )
                 return
-            message_prophet = message_prophet + "\n### Generating subqueries..."
-            await message.edit(content=message_prophet)
-            queries = generate_subqueries(
-                query=goal, limit=initial_subqueries_limit, model=model
-            )
-            message_prophet = message_prophet + "\n### Reranking subqueries..."
-            await message.edit(content=message_prophet)
-            queries = (
-                rerank_subqueries(queries=queries, goal=goal, model=model)[
-                    :subqueries_limit
-                ]
-                if initial_subqueries_limit > subqueries_limit
-                else queries
-            )
-
-            message_prophet = message_prophet + "\n### Searching the web..."
-            await message.edit(content=message_prophet)
-            search_results_with_queries = search(
-                queries,
-                lambda result: not result.url.startswith("https://www.youtube"),
-                tavily_api_key=tavily_api_key,
-            )
-
-            if not search_results_with_queries:
-                message_prophet = message_prophet = (
-                    "\n### No search results found for goal"
-                )
-                await message.edit(content=message_prophet)
-                return
-
-            message_prophet = message_prophet + "\n### Scraping web results..."
-            await message.edit(content=message_prophet)
-            scrape_args = [result for (_, result) in search_results_with_queries]
-
-            scraped = scrape_results(scrape_args)
-            scraped = [result for result in scraped if result.content != ""]
-
-            message_prophet = (
-                message_prophet + "\n### Performing similarity searches..."
-            )
-            await message.edit(content=message_prophet)
-
-            text_splitter = RecursiveCharacterTextSplitter(
-                separators=["\n\n", "\n", ". ", "  "],
-                chunk_size=scrape_content_split_chunk_size,
-                chunk_overlap=scrape_content_split_chunk_overlap,
-            )
-            collection = create_embeddings_from_results(scraped, text_splitter)
-
-            vector_result_texts: list[str] = []
-            for query in queries:
-                top_k_per_query_results = collection.similarity_search(
-                    query, k=top_k_per_query
-                )
-                vector_result_texts += [
-                    result.page_content
-                    for result in top_k_per_query_results
-                    if result.page_content not in vector_result_texts
-                ]
-
-            message_prophet = message_prophet + "\n### Preparing report..."
-            await message.edit(content=message_prophet)
-
-            report = prepare_report(goal, vector_result_texts, model=model)
-
-            message_prophet = message_prophet + "\n### Making prediction..."
-            await message.edit(content=message_prophet)
-
-            prediction = _make_prediction(
-                market_question=goal,
-                additional_information=report,
-                engine="gpt-4-0125-preview",
-                temperature=0.0,
-            )
-
-            if prediction.outcome_prediction == None:
-                message_prophet = (
-                    message_prophet + "\n### The agent failed to generate a prediction"
-                )
-                await message.edit(content=message_prophet)
-                return
-
-            outcome_prediction = cast(OutcomePrediction, prediction.outcome_prediction)
-            message_prophet = (
-                message_prophet
-                + f"\n# Probability: {outcome_prediction.p_yes * 100}%. Confidence: {outcome_prediction.confidence * 100}%"
-            )
-            await message.edit(content=message_prophet)
+            await handle_prediction(goal, message, message_prophet)
 
 
 intents = discord.Intents(messages=True)
 intents.message_content = True
 client = ProphetClient(intents=intents)
-client.run(discord_bot_token)
+client.run(discord_bot_token.get_secret_value())
